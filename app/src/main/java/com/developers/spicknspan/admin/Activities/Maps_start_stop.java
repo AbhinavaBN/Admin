479b543b458e0edee0e4c8b88b6e1fa0d265cdae
AbhinavaBN <43547397+AbhinavaBN@users.noreply.github.com>
2019-02-03 12:49:35 +0530

    Added new fetures for create location

---
+++ app/src/main/java/com/developers/spicknspan/admin/Activities/Maps_start_stop.java
@@ -0,0 +1,118 @@
+package com.developers.spicknspan.admin.Activities;
+
+import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.developers.spicknspan.admin.R;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.AutocompletePrediction;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.Arrays;
+import java.util.List;
+
+public class Maps_start_stop extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    int AUTOCOMPLETE_REQUEST_CODE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.e("", "Place: " + place.getName() + ", " + place.getId());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
+    }
+    public void func(){
+        try {
+
+            Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                    .build(Maps_start_stop.this);
+            //startActivity(intent);
+            startActivityForResult(intent, 1);
+        }
+        catch (Exception e){}
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    EditText t;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps_start_stop);
+        t = findViewById(R.id.start);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                func();
+            }
+        });
+// Set the fields to specify which types of place data to
+// return after the user has made a selection.
+
+        /*String preSearchEntry = "";
+        Intent intent = new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
+                .setBoundsBias(AppConstant.BOUNDS_INDIA)
+                .setFilter(typeFilter)
+                .zzlf(preSearchEntry)
+                .build(getActivity());
+        startActivityForResult(intent, PLACE_AUTOCOMPLETE_REQUEST_CODE);*/
+
+
+
+
+        /*startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+        List<Place.Field> fields = Arrays.asList(Place.Field.ID, Place.Field.NAME);
+
+// Start the autocomplete intent.
+        Intent intent = new Autocomplete.IntentBuilder(
+                AutocompleteActivityMode.FULLSCREEN, fields)
+                .build(this);
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE);*/
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
--- app/src/main/java/com/developers/spicknspan/admin/Activities/homepage.java
+++ app/src/main/java/com/developers/spicknspan/admin/Activities/homepage.java
@@ -1,5 +1,7 @@
 package com.developers.spicknspan.admin.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,22 +9,68 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.developers.spicknspan.admin.R;
 import com.google.android.gms.maps.SupportMapFragment;
 
+import java.lang.reflect.Field;
+import java.util.Locale;
+
 public class homepage extends android.support.v4.app.Fragment {
+
+    android.support.v7.widget.GridLayout Grid = null;
+
+    String layout_name = "ver_lay";
+
+    LinearLayout layout[] = new LinearLayout[3];
+
+    public static int getResId(String resName, Class<?> c) {
+
+        try {
+            Field idField = c.getDeclaredField(resName);
+            return idField.getInt(idField);
+        } catch (Exception e) {
+            Log.e("getResId",  e.toString());
+
+            return -1;
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_homepage, container, false);
 
+        Log.e("onclick", String.valueOf(layout.length));
+        //int ids[] = {R.id.ver_lay0, R.id.ver_lay1, R.id.ver_lay2};
+        View view = inflater.inflate(R.layout.activity_homepage, container, false);
+        for (int i = 0; i < layout.length; i++)
+        {
+            String name = layout_name + String.valueOf(i);
+            layout[i] = view.findViewById(getResId(name, R.id.class));
+            layout[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.e("onclick", view.toString());
+                    
+                    Intent intent = new Intent(getActivity(), Maps_start_stop.class);
+                    startActivity(intent);
+                    /*Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                            Uri.parse("http://maps.google.com/maps?saddr=20.344,34.34&daddr=20.5666,45.345"));
+                    startActivity(intent);*/
+                    /*String uri = String.format(Locale.ENGLISH, "geo:%f,%f", 16.60572, 77.50);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+                    startActivity(intent);*/
+                }
+            });
+        }
         return view;
+
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Log.e("homepage","oncreate");
     }
 }
